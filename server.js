b6393c8c6bb53a5646940cf00c209744ab5efec3
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-02-01 18:39:36 -0600

    added createNewAnimal() function and validateAnimal() function; Insomnia shows that the coee is working

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
-const { animals } = require('./data/animals')
+const { animals } = require('./data/animals');
+const { type } = require('os');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray){
     let personalityTraitsArray = [];
@@ -52,6 +61,40 @@ function findById(id, animalsArray){
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+
+    // use fs.writeFileSync() which is the synchronous version of fs.writeFile() and doesn't require a callback fuction
+    // we want to write our animals.json file in the data subdirectory _dirname which represents the directory of the file we execute the code in.
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+
+    // return finished code to post route for response
+    return animal;
+}
+
+function validateAnimal(animal){
+    if (!animal.name || typeof animal.name !== 'string'){
+        return false;
+    } 
+
+    if (!animal.species || typeof animal.species !== 'string'){
+        return false;
+    }
+
+    if (!animal.diet || typeof animal.diet !== 'string'){
+        return false;
+    }
+
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query){
@@ -68,6 +111,20 @@ app.get('/api/animals/:id', (req, res) => {
       res.sendStatus(404);
     }
   });
+
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
